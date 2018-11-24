99e03592bb567726d5761a041bd575458be56714
Charlie <charles.caron@tufts.edu>
2018-11-24 01:52:38 -0500

    Added mouseover effect on main menu, moved scripts into script folder

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOver : MonoBehaviour
+{
+
+    public int textChild;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject.Find("Canvas").transform.GetChild(textChild).gameObject.GetComponent<Text>().color = Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        GameObject.Find("Canvas").transform.GetChild(textChild).gameObject.GetComponent<Text>().color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        GameObject.Find("Canvas").transform.GetChild(textChild).gameObject.GetComponent<Text>().color = Color.black;
+    }
+}
