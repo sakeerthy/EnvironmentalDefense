0bfddd05086341e553e90bd2c0289629071f5c53
tjaeni01 <tuckerjaenicke@gmail.com>
2018-11-27 09:52:43 -0500

    tower menu script

---
+++ Assets/Scripts/TowerMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerMenu : MonoBehaviour {
+    
+    public Button yourButton;
+    public bool buttonisclicked;
+    
+
+    // Use this for initialization
+    void Start () {
+        Button btn = yourButton;
+        btn.onClick.AddListener(OpenMenu);
+        buttonisclicked = false;
+    }
+
+    void OpenMenu()
+    {
+        Debug.Log("You have clicked the button!");
+        buttonisclicked = true;
+    }
+
+
+    private void OnGUI()
+    {
+        //check if our button click boolean is true
+        if (buttonisclicked)
+        {
+            //create gui menu, refer to linked documentation to learn how to create gui elements via script, and learn about various styling techniques
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
