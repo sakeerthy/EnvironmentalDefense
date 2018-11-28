df82d86d69dc4541591e35761dc85c185693052d
sakeerthy <sakeerthy@gmail.com>
2018-11-27 20:09:53 -0500

    new stuff

---
+++ Assets/Scripts/upgrade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upgrade : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (GameObject.Find("CurrencyManager").GetComponent<currency>().upgradeTower(30)) {
+                Debug.Log("Upgraded!!");
+                transform.parent.gameObject.transform.localScale += new Vector3(1.5f, 1.5f, 1.5f);
+            } else {
+                Debug.Log("You poor");
+            }
+        }
+    }
+}
